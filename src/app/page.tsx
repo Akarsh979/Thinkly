bde86c054ca3c8cc69ce9063af366c68fefa5adb
Akarsh Singh <singhakarsh90@gmail.com>
2025-06-30 11:54:17 +0530

    Convex Working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 'use client'
 
 import { SignInButton, UserButton } from "@clerk/nextjs";
-import { Authenticated, AuthLoading, Unauthenticated } from "convex/react";
-
+import { Authenticated, AuthLoading, Unauthenticated, useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+
+  const createDocument = useMutation(api.documents.createDocument);
+  const documents = useQuery(api.documents.getDocuments)
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -13,6 +17,13 @@ export default function Home() {
       </Unauthenticated>
       <Authenticated>
         <UserButton />
+       
+      <button onClick={()=>{createDocument({title: "Hello World"})}}>Click me</button>
+
+      {documents?.map((doc)=>(
+        <div key={doc._id}>{doc.title}</div>
+      ))}
+
       </Authenticated>
       <AuthLoading>
         <p>Still loading</p>
